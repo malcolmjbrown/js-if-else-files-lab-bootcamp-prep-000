12c81f2384dfe254e18df6aae0e2be9eb19be2fb
malcolmjbrown1@yahoo.com <malcolmjbrown1@yahoo.com>
2018-07-12 00:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,3 @@ if (name === "Susan") {
 } else {
   console.log("Expected the name to be Susan")
 }
-
-if (height === "70") {
-      console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: 70")
-}
